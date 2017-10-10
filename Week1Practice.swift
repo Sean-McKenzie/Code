c3d27f2389427da0f4113753777ea3dba743b76d
Sean-McKenzie <sean_d_mckenzie@yahoo.com>
2017-10-10 14:17:24 -0700

    Add files via upload

---
+++ Week1Practice.swift
@@ -0,0 +1,97 @@
+// // Slide 4-Create Variable and specify type (ex: String)
+// var myFirstProgram: String
+// 	myFirstProgram = "Hello World"
+// 	print (myFirstProgram)
+
+// // Slide 5/6-Create Int Variable
+// var age: Int 
+// 	age = 27
+// 	print("You are \(age) years old")
+
+// // Slide 7-Create Variable called name, no type, with your name
+// var name = "Sean"
+// var lname = "McKenzie"
+// 	print("Great work \(name + " " + lname)!")
+
+// // Slide 8-Assign new value to variable
+// var feeling = "awesome"
+// 	//How are you feeling?
+// 	feeling = "Brr"
+// 	print("Your mood is \(feeling)!")
+
+// // Slide 9-Create a constant
+// // Constants start with let instead of var
+// let secondsPerMinute = 60
+// 	print(secondsPerMinute)
+
+// // Slide 11-Combine strings
+// //	declare constant
+// let appName = "Code Swift"
+// //	declare variables
+// 	var review = "is a fantastic app!"
+// //					combinations
+// 	var message = appName + review
+// 		print(message)
+// // Slide 12-Print function
+// var quote = "Practice makes perfect!"
+// 	print(quote)
+//  Slide 13-Sting interpolation
+//  var language = "Swift"
+// 	print("I am learning \(language)")
+//  Slide 14-17 
+// 	var company = "Apple"
+// 	let yearFounded = 1976
+// 	var str: String = "was founded in"
+// 	print(company, str, yearFounded)
+
+// Slide 18-21 Float
+// let celsius: Float //is assigned vlaue later, works once
+// let fahrenheit: Float = 50
+// celsius = (fahrenheit-32)/1.8
+// print("It is \(celsius) degrees Celsius")
+
+// Slide 22
+// var decision:Bool = true;
+// var ageOld:Int = 50;
+// var ageYoung:Int = 20;
+// comparison for Bool
+// decision = ( ageYoung > ageOld )
+// print("The value of decision is \(decision)")
+// Added
+// var decision:Bool = true;
+// var ageOld:Int = 50;
+// var ageYoung:Int = 20;
+// var ageTeen = 13;
+// var myAge = 17;
+// var weather: String = "Sunny";
+// comparison for Bool
+//                                  ANDv
+// decision = ( myAge > ageTeen ) && (myAge < ageYoung);
+//                               ORv
+// decision = ( myAge > ageTeen ) || (myAge < ageYoung);
+// var decision2: Bool = (weather == "Sunny");
+// print("The value of decision is \(decision)");
+// print("Weather comparison is \(decision2)");
+// var outcome: Bool = (decision) || (decision2);
+// print ("decision or decision2 is \(outcome)")
+// var temperature:Double = 75
+// var decision:Bool = temperature >= 70
+// var isRainy:Bool = true
+// if (temperature >= 70)
+// {
+// 	if (isRainy)
+// 	{
+// 		print("It's raining")
+// 	}
+// 	else 
+// 	{
+// 		print("It's not raining")
+// 		print ("The Weather is enjoyable")
+// 	}
+
+
+// }
+// else{
+// 	print ("Better stay in")
+// }
+
